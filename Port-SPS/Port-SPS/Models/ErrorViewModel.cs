088b83cc1602e359a46074f5216805926737b5eb
Matyáš Čepelka <cepelkam24@spstrutnov.cz>
2026-06-15 14:32:50 +0200

    hrube opraveno

--- Port-SPS/Port-SPS/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Port_SPS.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Port-SPS/Port-SPS/Program.cs
+++ Port-SPS/Port-SPS/Program.cs
@@ -1,57 +1,7 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Port_SPS.Data;
-using Port_SPS.Models;
-using Port_SPS.Services;
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Add DbContext
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
-    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-// Add Identity
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
-{
-    options.SignIn.RequireConfirmedEmail = false;
-    options.Password.RequiredLength = 8;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
-})
-.AddEntityFrameworkStores<ApplicationDbContext>()
-.AddDefaultTokenProviders();
-
-// Add Email Validator
-builder.Services.AddScoped<IEmailValidator, SchoolEmailValidator>();
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
+app.UseDefaultFiles();
 app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapRazorPages();
-app.MapStaticAssets();
 
 app.Run();
